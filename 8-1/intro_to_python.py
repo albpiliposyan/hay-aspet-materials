6ad4b20a185e8be7e99eaffc68301e07e2bf2873
Albert Piliposyan <albpiliposyan@gmail.com>
2026-03-10 02:41:16 +0400

    5th grade - bookmarks

---
+++ 8-1/intro_to_python.py
@@ -0,0 +1,24 @@
+# print(Hello world)
+# print("Hello, World!")
+
+
+name = "Վահե"
+print("Ողջույն,", name)
+
+year = int(input(f"Ո՞ր թվականին ես ծնվել, {name}. \n"))
+
+age = 2026 - year
+print(f"Դու {age} տարեկան ես։")
+
+
+# rate = 377
+# 
+# dollar = float(input("Որքա՞ն դոլար եք ցանկանում փոխանակել. "))
+# 
+# dram = dollar * rate
+# print("Դուք կստանաք", dram, "դրամ։")
+
+# import random
+# number = random.randint(1,10)
+# guess = int(input("Գուշակիր թիվը: "))
+# print("Ես պահել էի ", number, "թիվը")
